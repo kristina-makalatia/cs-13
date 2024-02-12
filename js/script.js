93da21eaad611c08c1365d284f0fa83deadbb457
kristina-makalatia <33975427+kristina-makalatia@users.noreply.github.com>
2024-02-12 17:58:54 +0400

    add script

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//variables
+//string number boolean null undefined
+let userName = "Anna"; //string
+let userAge = 25; //number
+let userStatus = false; //boolean
+let x = null;//null
+let y; //undefined
+
